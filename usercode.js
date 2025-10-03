e5a74058d03697e0fde8583f28bb90c6e65a28da
VandalVape <133953221+VandalVape@users.noreply.github.com>
2025-10-03 13:13:02 +0000

    modified:   usercode.js

--- usercode.js
+++ usercode.js
@@ -1,3 +1,4 @@
 // usercode.js
-const { handle } = require('./wrap');
-module.exports = { handle };
+const { handle } = require('./wrap');   // ваш робочий wrap.js з бізнес-логікою
+exports.handle = handle;                // варіант 1
+module.exports = { handle };            // варіант 2 (обидва лишаємо)
